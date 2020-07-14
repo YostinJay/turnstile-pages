0e976d76807c74fcdfd6bcda5d50f7e9272355f8
jjaaaayyyy <61051818+jjaaaayyyy@users.noreply.github.com>
2020-07-14 22:23:55 +0800

    Add files via upload

---
+++ new-script.js
@@ -0,0 +1,41 @@
+$(document).ready(function () {
+    $("#input-1").click(function () {
+        $("#myDIV").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#input-2").click(function () {
+        $("#myDIVXS").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#input-3").click(function () {
+        $("#myDIVSub").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#input-4").click(function () {
+        $("#myDIVSub-2").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#input-5").click(function () {
+        $("#myDIVSub-3").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#input-6").click(function () {
+        $("#myDIVSub-4").toggle();
+    });
+});
+
+$(document).ready(function () {
+    $("#input-7").click(function () {
+        $("#myDIVSub-4").toggle();
+    });
+});
